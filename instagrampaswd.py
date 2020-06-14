1615c1e33a328593c47b2adef772cc4a3aa9f92e
coderqmo <66912703+coderqmo@users.noreply.github.com>
2020-06-14 17:55:58 +0300

    Add files via upload

---
+++ instagrampaswd.py
@@ -0,0 +1,35 @@
+#**CODE BY Qmo****#
+#Calani sikerim
+##INSTAGRAM : _ardatunaa
+
+
+import random
+import time
+pass_generator = 'abcdefghijklmnopqrstuvwxyz12345678910*)$($+$838282(2+$;$!$))'
+
+number = input("Sayi Adeti: ")
+number = int(number)
+
+lenght = input("Kac Hane: ")
+lenght = int(lenght)
+
+for x in range(number):
+  password= ''
+  for g in range(lenght):
+     password += random.choice(pass_generator)
+     
+print("Olusturuluyor...")
+time.sleep(2)
+print("%50")
+print("")
+print("")
+time.sleep(1)
+print"_ardatunaa follow me pls"
+print("")
+print("")
+time.sleep(1.5)
+print"%100"
+print("")
+print("")
+time.sleep(1.3)
+print("Your Password ", password )
